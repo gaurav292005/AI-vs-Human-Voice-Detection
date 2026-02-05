3a2b252eb0c9075a5ea960fab6358a82968dae7f
Gaurav Sharma <85092192+gaurav292005@users.noreply.github.com>
2026-02-05 17:52:16 +0530

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>Deployment Successful 🚀</h1>
+      <p>Your Railway + Docker + Next.js setup is working.</p>
+    </main>
+  );
+}
